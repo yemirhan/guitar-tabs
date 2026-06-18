1e1326d2bd49ce3d19a7b82256d5137cbd822a7d
Yusuf Emirhan Şahin <syusufemirhan@gmail.com>
2026-06-18 10:55:08 +0300

    chore: update mobile app configuration

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,10 +1,15 @@
+import { useColorScheme } from 'react-native'
 import { Stack } from 'expo-router/stack'
+import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router'
 
 export default function Layout() {
+  const colorScheme = useColorScheme()
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: 'Library', headerLargeTitle: true }} />
-      <Stack.Screen name="player" options={{ title: '' }} />
-    </Stack>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: 'Library', headerLargeTitle: true }} />
+        <Stack.Screen name="player" options={{ title: '' }} />
+      </Stack>
+    </ThemeProvider>
   )
 }
